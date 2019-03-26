8bc7007a847c26807cb7dd6c41da90ab09c18b87
Yumin Gui <erichui329@gmail.com>
2019-03-25 23:22:56 -0700

    continue to optimize logogrid, and tooltip is not done yet

--- src/components/common/LogoGrid.vue
+++ src/components/common/LogoGrid.vue
@@ -3,10 +3,15 @@
     <v-layout row wrap>
       <template v-for="i in [1,2,3,4,5,6,7,8,9,10,11,12]">
         <v-flex :key="i" xs4 sm2>
-          <v-layout justify-center>
-            <v-avatar :tile="true" color="grey lighten-4">
-              <img src="@/assets/logo.png" alt="avatar">
-            </v-avatar>
+          <v-layout row justify-center>
+            <v-flex xs8 class="text-xs-center">
+              <v-avatar :tile="true" color="grey lighten-4">
+                <img src="@/assets/logo.png" alt="avatar">
+              </v-avatar>
+            </v-flex>
+            <v-flex xs4>
+              <v-checkbox></v-checkbox>
+            </v-flex>
           </v-layout>
         </v-flex>
       </template>
