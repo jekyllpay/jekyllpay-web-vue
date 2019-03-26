2bea082f11918fff12d5d0201ae280cee9deb353
Yumin Gui <erichui329@gmail.com>
2019-03-25 22:27:36 -0700

    optimize logo-grid, logo is now centered

--- src/components/common/LogoGrid.vue
+++ src/components/common/LogoGrid.vue
@@ -1,10 +1,14 @@
 <template>
   <v-flex xs12 sm12>
     <v-layout row wrap>
-      <template v-for="i in [1,2,3,4,5,6]">
-        <v-avatar :tile="true" size="80px" color="grey lighten-4" :key="i">
-          <img src="@/assets/logo.png" alt="avatar">
-        </v-avatar>
+      <template v-for="i in [1,2,3,4,5,6,7,8,9,10,11,12]">
+        <v-flex :key="i" xs4 sm2>
+          <v-layout justify-center>
+            <v-avatar :tile="true" color="grey lighten-4">
+              <img src="@/assets/logo.png" alt="avatar">
+            </v-avatar>
+          </v-layout>
+        </v-flex>
       </template>
     </v-layout>
   </v-flex>
