9a3890cec22a13aa394bf765609f08e435271ad6
medmin <erichui329@gmail.com>
2019-06-05 20:45:58 -0700

    run lint

--- src/utils/gateway-apis/unionpay-api.js
+++ src/utils/gateway-apis/unionpay-api.js
@@ -1,4 +1,1 @@
-import axios from 'axios'
-
-// scheme 'http' or "https" is required in BASE_URL
-let BASE_URL = process.env.NODE_ENV === "production" ? process.env.BASE_URL : 'http://localhost:8080';
+import  { BASE_URL, axios }  from '@/utils/apis/index';
